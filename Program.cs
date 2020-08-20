8239fe4128df64e5550f1a83e2eb41ba6ca78124
mgrupa89 <69479947+mgrupa89@users.noreply.github.com>
2020-08-20 13:01:37 -0500

    First commit for projects

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are " + age + " years old.");
+            Console.WriteLine(age + " isn't that old!");
+        }
+    }
+}
